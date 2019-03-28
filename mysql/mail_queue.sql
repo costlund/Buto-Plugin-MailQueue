81686643f2ae1766026dcfc2900c2041532a4176
Claes Ö <claes@stenbergit.se>
2019-03-28 09:41:58 +0100

    Add sql file.

---
+++ mysql/mail_queue.sql
@@ -0,0 +1,31 @@
+
+
+
+#
+select * from mailqueue_queue order by created_at desc;
+
+
+# Sent emails.
+select 
+m.mail_to,
+s.created_at,
+m.subject,
+m.body,
+m.tag,
+m.error_text
+from mailqueue_queue as m
+inner join mailqueue_send as s on m.send_id=s.id
+order by s.created_at desc
+;
+
+# Not sent emails.
+select 
+m.mail_to,
+m.created_at,
+m.subject,
+m.body,
+m.tag
+from mailqueue_queue as m
+where isnull(m.send_id)
+order by m.created_at desc
+;
